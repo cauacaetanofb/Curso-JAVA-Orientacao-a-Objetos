390011657225b1e425de43aed601aeb7bf48226b
Cauã Caetano <163075994+cauacaetanofb@users.noreply.github.com>
2025-02-26 15:29:47 -0300

    Área, perímetro e diagonal de um retângulo

---
+++ retangulo/src/entities/Rectangle.java
@@ -0,0 +1,20 @@
+package entities;
+
+public class Rectangle {
+	
+	public double a;
+	public double b;
+	
+	public double area() {
+		return a * b;
+	}
+	
+	public double perimeter() {
+		return (2 * a) + (2 * b);
+	}
+	
+	public double diagonal() {
+		return Math.sqrt((a * a) + (b * b));
+	}
+	
+}
